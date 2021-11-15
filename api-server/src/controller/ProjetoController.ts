0247878271e59006e00777d2469cec070b014be1
MatheusKunnen <matheuskl@accionproactiva.com>
2021-11-15 14:51:30 -0300

    Obter todos os projetos de um usuario (colaborador & gerenciador)

--- api-server/src/controller/ProjetoController.ts
+++ api-server/src/controller/ProjetoController.ts
@@ -1,3 +1,4 @@
+import BancoDeDados from '../BancoDeDados';
 import ColaboradorProjeto from '../model/ColaboradorProjeto';
 import Pessoa from '../model/Pessoa';
 import Projeto from '../model/Projeto';
@@ -15,10 +16,46 @@ export default class ProjetoController {
     descricao: string,
     gerenciador: number
   ) {
-    return Projeto.create({ nome, descricao, gerenciador });
+    const transaction = await BancoDeDados.getInstance()
+      .getDbInstance()
+      .transaction();
+    try {
+      const projeto = await Projeto.create(
+        { nome, descricao, gerenciador },
+        { transaction, include: [Projeto.Tarefa] }
+      );
+      await ColaboradorProjeto.create(
+        {
+          id_projeto: projeto.id_projeto,
+          id_pessoa: projeto.gerenciador,
+        },
+        { transaction }
+      );
+      await transaction.commit();
+      return projeto;
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
   }
 
-  static async getProjetoByID(gerenciador: number, id_projeto: number) {
+  static async getProjetos(id_pessoa?: number) {
+    const where = typeof id_pessoa === 'undefined' ? {} : { id_pessoa };
+    const id_projetos = await ColaboradorProjeto.findAll({
+      where,
+      attributes: ['id_projeto'],
+    });
+
+    if (id_projetos === null) return [];
+
+    const projetos = await Promise.all(
+      id_projetos.map(({ id_projeto }) =>
+        Projeto.findOne({ where: { id_projeto } })
+      )
+    );
+    return projetos.filter((projeto) => projeto !== null);
+  }
+  static async getProjetoByID(id_projeto: number) {
     return Projeto.findOne({
       where: { id_projeto },
       include: [Projeto.Tarefa],
@@ -26,14 +63,13 @@ export default class ProjetoController {
   }
 
   static async adicionarColaboradorProjeto(
-    gerenciador: number,
     id_projeto: number,
     colaborador: number
   ) {
-    let projeto = await Projeto.findOne({ where: { id_projeto, gerenciador } });
+    let projeto = await Projeto.findOne({ where: { id_projeto } });
     if (projeto === null) return null;
     await ColaboradorProjeto.create({ id_projeto, id_pessoa: colaborador });
-    return ProjetoController.getProjetoByID(gerenciador, id_projeto);
+    return ProjetoController.getProjetoByID(id_projeto);
   }
 
   static async getColaboradoresProjeto(id_projeto: number) {
--- api-server/src/router/ProjetoRouter.ts
+++ api-server/src/router/ProjetoRouter.ts
@@ -3,6 +3,7 @@ import Router from './Router';
 import ProjetoController from '../controller/ProjetoController';
 import UsuarioRouter from './UsuarioRouter';
 import asyncHandler from '../middleware/asyncHandler';
+import Projeto from '../model/Projeto';
 
 export default class ProjetoRouter extends Router {
   constructor(app: express.Application) {
@@ -14,11 +15,16 @@ export default class ProjetoRouter extends Router {
     ProjetoController.init();
     // Inicia rutas
     const router = express.Router();
-    router.post(
-      '/',
-      UsuarioRouter.checkLogin,
-      asyncHandler(ProjetoRouter.criarProjeto)
-    );
+    router
+      .route('/')
+      .post(
+        asyncHandler(UsuarioRouter.checkLogin),
+        asyncHandler(ProjetoRouter.criarProjeto)
+      )
+      .get(
+        asyncHandler(UsuarioRouter.checkLogin),
+        asyncHandler(ProjetoRouter.getProjetos)
+      );
     router.get(
       '/:id_projeto',
       UsuarioRouter.checkLogin,
@@ -62,19 +68,27 @@ export default class ProjetoRouter extends Router {
     const colaboradores = await ProjetoController.getColaboradoresProjeto(
       projeto.id_projeto
     );
-    return res.status(201).json({ data: { ...projeto.get(), colaboradores } });
+    return res
+      .status(201)
+      .json({ data: { ...projeto.get(), colaboradores, tarefas: [] } });
+  }
+
+  static async getProjetos(req: Request, res: Response) {
+    // @ts-ignore
+    const id_usuario = Number(req.usuario.id_pessoa);
+    const projetos = await ProjetoController.getProjetos(id_usuario);
+
+    return res.status(200).json({ data: projetos });
   }
 
   static async getProjetoById(req: Request, res: Response) {
     const id_projeto = req.params.id_projeto;
+    // @ts-ignore
+    const id_usuario = Number(req.usuario.id_pessoa);
     if (typeof id_projeto === 'undefined')
       return res.status(400).json('ID inválido');
 
-    const projeto = await ProjetoController.getProjetoByID(
-      // @ts-ignore
-      req.usuario!.id_usuario,
-      Number(id_projeto)
-    );
+    const projeto = await ProjetoController.getProjetoByID(Number(id_projeto));
 
     if (projeto === null)
       return res
@@ -83,31 +97,39 @@ export default class ProjetoRouter extends Router {
     const colaboradores = await ProjetoController.getColaboradoresProjeto(
       projeto.id_projeto
     );
+    if (projeto.gerenciador !== id_usuario) {
+      let colaborador = false;
+      colaboradores.map(({ id_pessoa }) => {
+        if (id_pessoa === id_usuario) colaborador = true;
+      });
+      if (!colaborador) return res.status(403).json();
+    }
+
     return res.status(200).json({ data: { ...projeto.get(), colaboradores } });
   }
   static async adicionarColaborador(req: Request, res: Response) {
     const id_projeto = req.params.id_projeto;
     const id_pessoa = req.params.id_pessoa;
+    // @ts-ignore
+    const id_pessoa_auth = Number(req.usuario.id_pessoa);
     if (typeof id_projeto === 'undefined' || typeof id_pessoa === 'undefined')
       return res.status(400).json('ID inválido');
 
-    let projeto = await ProjetoController.getProjetoByID(
-      // @ts-ignore
-      req.usuario!.id_usuario,
-      Number(id_projeto)
-    );
+    let projeto = await ProjetoController.getProjetoByID(Number(id_projeto));
 
     if (projeto === null)
       return res
         .status(404)
         .json({ error: `Projeto ${id_projeto} não encontrado` });
 
+    if (projeto.gerenciador !== id_pessoa_auth) {
+      return res.status(403).json();
+    }
     let projeto2 = await ProjetoController.adicionarColaboradorProjeto(
-      // @ts-ignore
-      req.usuario!.id_usuario,
       Number(id_projeto),
       Number(id_pessoa)
     );
+
     if (projeto2 === null) return res.status(500).json();
     const colaboradores = await ProjetoController.getColaboradoresProjeto(
       projeto2.id_projeto
